10124ab35993803fd6f00422ca9379e2d012a404
SmileTR <tr580230@163.com>
2019-07-02 17:13:31 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,44 @@
+const TerserPlugin = require('terser-webpack-plugin')
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  publicPath: isProduction ? '/smile-love' : '/',
+
+  outputDir: 'dist',
+
+  assetsDir: 'static',
+
+  filenameHashing: true,
+
+  productionSourceMap: false,
+
+  configureWebpack: (config) => {
+    if (isProduction) {
+      config.optimization.minimizer.push(
+        new TerserPlugin({
+          // 是否过滤
+          chunkFilter: (chunk) => {
+            // Exclude uglification for the `vendor` chunk
+            if (chunk.name === 'vendor') {
+              return false
+            }
+            return true
+          },
+          // 是否缓存
+          cache: true,
+          // 是否多进程打包
+          parallel: true,
+          // 提取注释
+          extractComments: 'all'
+        })
+      )
+    }
+  },
+
+  chainWebpack: (config) => {
+
+  },
+
   css: {
     loaderOptions: {
       stylus: {
@@ -9,6 +49,7 @@ module.exports = {
       }
     }
   },
+
   pluginOptions: {
     'cube-ui': {
       postCompile: true,
